a9a95f0b30b2f73badb914156083cc957f9d8bcd
ashketchem <65618601+ashketchem@users.noreply.github.com>
2024-07-11 14:24:47 +0000

    made a webpage

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const girdSizeInput = document.querySelector("#gird-size");
 const generateGridButton = document.querySelector("#generate-grid");
 const girdContainer = document.querySelector(".grid-container");
+
 generateGridButton.addEventListener("click", () => {
   const girdSize = parseInt(girdSizeInput.value);
+  
   const numGirdItems = girdSize * girdSize;
-  if (girdSize < 10 || girdSize > 100) {
-    return alert("invalid input, enter a number between 10 to 100.");
+  if (girdSize < 4 || girdSize > 100) {
+    return alert("invalid input, enter a number between 4 to 100.");
   } else {
     girdContainer.innerHTML = '';
     for (let i = 0; i < numGirdItems; i++) {
@@ -13,16 +15,20 @@ generateGridButton.addEventListener("click", () => {
       girdItem.classList.add("grid-item");
       girdContainer.appendChild(girdItem);
       girdItem.addEventListener("click", () => {
+        
         const color = generateRandomColor();
         girdItem.style.backgroundColor = color;
       });
     }
-    const girdItemWidth = `calc(100% / ${girdSize})`;
-    const girdItemHeight = `calc(100% / ${girdSize})`;
+    const girdItemWidth = `calc(100 / ${girdSize})`;
+    const girdItemHeight = `calc(100 / ${girdSize})`;
     girdContainer.style.setProperty('--gird-item-width', girdItemWidth);
     girdContainer.style.setProperty('--gird-item-height', girdItemHeight);
   }
 });
 generateRandomColor = () => {
-  return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-}
+  let r,b,g;
+  r = Math.floor(Math.random() * 256);
+  g = Math.floor(Math.random() * 256);
+  b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
